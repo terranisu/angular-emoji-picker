763dd3a6746a514fb9bdfa9363734c09253da1c2
Vadim Kadnikov <v.kadnikov@corp.mail.ru>
2015-04-22 20:16:41 +0300

    Feature: added the ability to remove emoji #2

--- dist/js/emoji-picker.js
+++ dist/js/emoji-picker.js
@@ -1374,6 +1374,39 @@ angular.module('vkEmojiPicker').directive('emojiPopover', [
   }
 ]);
 
+angular.module('vkEmojiPicker').directive('emojiRemovable', [function () {
+  return {
+    restrict: 'A',
+    scope: {
+      model: '=emojiRemovable'
+    },
+    link: function ($scope, element) {
+      var rebindClick = function (_newLength, _oldLength) {
+        var emojis = element[0].querySelectorAll('i.emoji-picker');
+        angular.forEach(emojis, function (emoji) {
+          var emojiElement = angular.element(emoji);
+          emojiElement.off();
+          emojiElement.on('click', function () {
+            $scope.model = $scope.model.replace(emojiElement.attr('title'), '');
+            emojiElement.off();
+            emojiElement.remove();
+            $scope.$apply();
+          });
+        });
+      };
+
+      $scope.$watch(
+        function () { return element[0].querySelectorAll('i.emoji-picker').length; },
+        rebindClick
+      );
+
+      $scope.$on('$destroy', function () {
+        element.remove();
+      });
+    }
+  };
+}]);
+
 angular.module('vkEmojiPicker').filter('emojify', [
   'EmojiGroups', function (emojiGroups) {
     var regex = new RegExp(':(' + emojiGroups.all.join('|') + '):', 'g');
